c33c91961c31db52a8305d16516f46ac8ddada6e
Laura Smith <kersytelaura@gmail.com>
2021-03-18 19:07:08 -0400

    Formatting

--- login.test.js
+++ login.test.js
@@ -1,8 +1,7 @@
-require('chromedriver');
-const {Builder, By, Key, until} = require('selenium-webdriver');
+require('chromedriver')
+const {Builder, By, Key, until} = require('selenium-webdriver')
 
 const rootURL = 'https://www.donorperfect.net/prod/Logon'
-
  
 beforeAll(async () => {
     driver = await new Builder().forBrowser('chrome').build()
@@ -10,65 +9,49 @@ beforeAll(async () => {
   
 afterAll(async () => driver.quit())
 
+/* Tests */
 describe("Login Function", () => {
-
-test('initialises the context', async () => {
+  
+  test('initialises the context', async () => {
     await driver.get(rootURL)
   })
 
-test('should display error message with blank user ID', async() => {
+  test('should display error message with blank user ID', async() => {
     await driver.navigate().to("https://www.donorperfect.net/prod/Logon/");
     await driver.findElement(By.id("txtUserID")).sendKeys("");
     await driver.findElement(By.id("txtPassword")).sendKeys("123");
     await driver.findElement(By.id("submit1")).click();
-    await driver.findElement(By.id("divErrors")).getText().then (value => {
-      console.log(value);
-      expect(value).toEqual(
-        expect.stringContaining("You must specify a non-blank User ID")
-      );
-    });
-});
+    await driver.findElement(By.id("divErrors")).getText().
+      then(value => {expect(value).toEqual(expect.stringContaining("You must specify a non-blank User ID"))})
+  })
 
-test('should display error message with blank password', async() => {
-  await driver.navigate().to("https://www.donorperfect.net/prod/Logon/");
-  await driver.findElement(By.id("txtUserID")).sendKeys("123456");
-  await driver.findElement(By.id("txtPassword")).sendKeys("");
-  await driver.findElement(By.id("submit1")).click();
-  await driver.findElement(By.id("divErrors")).getText().then (value => {
-    console.log(value);
-    expect(value).toEqual(
-      expect.stringContaining("You must specify a non-blank Password")
-    );
-  });
-  
-}) 
+  test('should display error message with blank password', async() => {
+    await driver.navigate().to("https://www.donorperfect.net/prod/Logon/");
+    await driver.findElement(By.id("txtUserID")).sendKeys("123456");
+    await driver.findElement(By.id("txtPassword")).sendKeys("");
+    await driver.findElement(By.id("submit1")).click();
+    await driver.findElement(By.id("divErrors")).getText().then(value => {
+      expect(value).toEqual(expect.stringContaining("You must specify a non-blank Password"))})
+  }) 
 
- test('should login with valid username and password', async() => {
+  test('should login with valid username and password', async() => {
     await driver.navigate().to("https://www.donorperfect.net/prod/Logon/");
     await driver.findElement(By.id("txtUserID")).sendKeys(""); //insert valid user id here
     await driver.findElement(By.id("txtPassword")).sendKeys("");  //insert valid password here
     await driver.findElement(By.id("submit1")).click();
     await driver.getCurrentUrl().then(value => {
-      expect(value).toEqual(
-         expect.stringContaining("/prod/Sidebar")
-      );
-    }) 
- }); 
+      expect(value).toEqual(expect.stringContaining("/prod/Sidebar"))}) 
+  }) 
 
-
-
-test('should redirect with invalid user id and password', async() => {
+  test('should redirect with invalid user id and password', async() => {
     await driver.navigate().to("https://www.donorperfect.net/prod/Logon/");
     await driver.findElement(By.id("txtUserID")).sendKeys("notarealusername");
     await driver.findElement(By.id("txtPassword")).sendKeys("notarealpassword");
     await driver.findElement(By.id("submit1")).click();
     await driver.getCurrentUrl().then(value => {
-      expect(value).toEqual(rootURL+"/Account/InvalidLogin");
-    }) 
+      expect(value).toEqual(rootURL+"/Account/InvalidLogin")})
   }) 
-
-});
-
+})
 
 describe("Reset Username or Password", () => {
 
@@ -80,20 +63,16 @@ describe("Reset Username or Password", () => {
     await driver.navigate().to("https://www.donorperfect.net/prod/Logon/");
     await driver.findElement(By.id("forgot_password")).click();
     await driver.getCurrentUrl().then(value => {
-      expect(value).toEqual(rootURL+"/Account/PasswordReset");
-    }) 
+      expect(value).toEqual(rootURL+"/Account/PasswordReset")}) 
   }) 
 
   test('should redirect to remind user id page', async() => {
     await driver.navigate().to("https://www.donorperfect.net/prod/Logon/");
     await driver.findElement(By.id("forgot_userid")).click();
     await driver.getCurrentUrl().then(value => {
-      expect(value).toEqual(rootURL+"/Account/UserIdReminder");
-    }) 
+      expect(value).toEqual(rootURL+"/Account/UserIdReminder")}) 
   }) 
-
-});
-
+})
 
 describe("External Links", () => {
 
@@ -105,9 +84,6 @@ describe("External Links", () => {
     await driver.navigate().to("https://www.donorperfect.net/prod/Logon/");
     await driver.findElement(By.id("logonAdLink")).click();
     await driver.getCurrentUrl().then(value => {
-      expect(value).toEqual("https://www.donorperfect.com/clients/rewardsprogram/");
-    }) 
+      expect(value).toEqual("https://www.donorperfect.com/clients/rewardsprogram/")}) 
   }) 
-
-
-});
+})
